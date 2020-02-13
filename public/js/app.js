4dc2a7c60a8371ebd3e235c65f949c3343da8d73
Ayodeji Afolabi <ayodejigreatest@gmail.com>
2020-02-13 19:27:46 +0100

    Setup add for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + place_name).then((response) => {
+    fetch('/weather?address=' + place_name).then((response) => {
     response.json().then((data) => {
         if(data.error){
             messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.set('view engine', 'hbs')
@@ -80,6 +80,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server don start o, check port:3000')
+app.listen(port, () => {
+    console.log('Server don start o, check port: ' + port)
 })
